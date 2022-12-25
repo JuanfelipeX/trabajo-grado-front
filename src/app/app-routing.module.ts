bf79a234150d7783453c703219d9a8fd71a9ee9a
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-12-25 17:45:55 -0500

    haciendo las rutas de los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorNoEncontradoComponent } from './components/error-no-encontrado/error-no-encontrado.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: ErrorNoEncontradoComponent }, //Sitio Web No Encontrado 404
+  { path: 'iniciar-sesion', component: IniciarSesionComponent },
+  { path: 'registrarse', component: RegistrarseComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
