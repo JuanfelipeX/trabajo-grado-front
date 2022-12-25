e163d270dbd71da8c738e22d8aa601aa1882483d
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-12-25 18:14:09 -0500

    instalando dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
 import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 import { ErrorNoEncontradoComponent } from './components/error-no-encontrado/error-no-encontrado.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     IniciarSesionComponent,
     RegistrarseComponent,
-    ErrorNoEncontradoComponent
+    ErrorNoEncontradoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
