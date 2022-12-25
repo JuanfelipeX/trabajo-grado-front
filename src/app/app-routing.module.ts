f6953212511959e0727e79e1aba1d926fe0074a6
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-12-25 18:14:20 -0500

    arreglando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ErrorNoEncontradoComponent } from './components/error-no-encontrado/error-no-encontrado.component';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
 import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 
 const routes: Routes = [
-  { path: '**', component: ErrorNoEncontradoComponent }, //Sitio Web No Encontrado 404
   { path: 'iniciar-sesion', component: IniciarSesionComponent },
   { path: 'registrarse', component: RegistrarseComponent },
+
+  { path: '**', component: ErrorNoEncontradoComponent }, //Sitio Web No Encontrado 404
+  { path: '', redirectTo: 'registrarse', pathMatch: 'full' },
 ];
 
 @NgModule({
