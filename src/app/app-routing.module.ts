2809e4fd766abf0a8766ff3af1145963e44951a4
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-02-07 16:32:43 -0500

    agregando ruta de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorNoEncontradoComponent } from './components/error-no-encontrado/error-no-encontrado.component';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 
 const routes: Routes = [
   { path: 'iniciar-sesion', component: IniciarSesionComponent },
   { path: 'registrarse', component: RegistrarseComponent },
+  { path: 'inicio', component: InicioComponent },
   { path: '', redirectTo: 'registrarse', pathMatch: 'full' },
   { path: '**', component: ErrorNoEncontradoComponent }, //Sitio Web No Encontrado 404
 ];
