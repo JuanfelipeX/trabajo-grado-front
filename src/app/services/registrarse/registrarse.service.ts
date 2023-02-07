592a2352e3a562dc9d79c133b5054253cc79cecd
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-02-07 16:04:20 -0500

    haciendo el servicio de registrase

--- src/app/services/registrarse/registrarse.service.ts
+++ src/app/services/registrarse/registrarse.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfigurationService } from '../configuration.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RegistrarseService {
+  private URL_BASE: string;
 
-  constructor() { }
+  constructor(
+    private configuration: ConfigurationService,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.URL_BASE = this.configuration.getUrlBase();
+  }
+
+  crearUsuarios(data: any) {
+    return this.http.post(this.URL_BASE + 'usuarios', data);
+  }
+
+  obtenerUsuarios() {
+    return this.http.get<any>(this.URL_BASE + 'usuarios');
+  }
+
+  obtenerUsuariosId(id: any) {
+    return this.http.get<any>(this.URL_BASE + 'usuarios' + '/' + id);
+  }
+
+  editarUsuarios(data: any, id: number) {
+    return this.http.put(this.URL_BASE + 'usuarios' + '/' + id, data);
+  }
+
+  eliminarUsuarios(id: number) {
+    return this.http.delete(this.URL_BASE + 'usuarios' + '/' + id);
+  }
 }
