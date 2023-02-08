4b0137de2edce479071c3cd65218143596fac98d
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-02-08 16:31:41 -0500

    obteniendo el id del usuario y comprobando si el correo y contrasena so iguales

--- src/app/components/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/components/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegistrarseService } from 'src/app/services/registrarse/registrarse.service';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -6,13 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iniciar-sesion.component.css'],
 })
 export class IniciarSesionComponent implements OnInit {
+  formulario: any = {};
 
-  formulario: any = {
-    correo: null,
-    contrasena: null,
-  };
+  formularioUserByid: any = {};
 
-  constructor() {}
+  constructor(private registrarseService: RegistrarseService) {}
 
   ngOnInit(): void {}
+
+  getUsuarioById() {
+    this.registrarseService.obtenerUsuariosId(this.formulario.id).subscribe({
+      next: (data) => {
+        this.formularioUserByid = data;
+      },
+      error: (err) => {},
+    });
+  }
+
+  iniciarSesion() {
+    this.getUsuarioById();
+
+    setTimeout(() => {
+      if (
+        this.formulario.email == this.formularioUserByid.email &&
+        this.formulario.contrasena == this.formularioUserByid.contrasena
+      ) {
+        console.log('paso');
+      } else {
+        console.log('no pasa');
+      }
+    }, 500);
+  }
 }
