afc70ebffbcba4174b99299bf423ec46933c74aa
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-02-18 09:26:43 -0500

    haciendo valiador de getItem contrasena

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.verifyLooged();
   }
 
+  ngOnInit(): void {}
+
+  verifyLooged() {
+    if (localStorage.getItem('contrasena')) {
+    } else {
+      this.router.navigateByUrl('iniciar-sesion');
+    }
+  }
 }
