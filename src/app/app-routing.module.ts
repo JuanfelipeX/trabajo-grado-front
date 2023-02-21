9b6ef2e14f61e7be6e97f22ed23edb6c174c181c
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-02-20 19:06:15 -0500

    agregando la ruta para registrar herramientas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorNoEncontradoComponent } from './components/error-no-encontrado/error-no-encontrado.component';
+import { HerramientaComponent } from './components/herramienta/herramienta.component';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { RegistrarseComponent } from './components/registrarse/registrarse.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'iniciar-sesion', component: IniciarSesionComponent },
   { path: 'registrarse', component: RegistrarseComponent },
   { path: 'inicio', component: InicioComponent },
+  { path: 'herramienta', component: HerramientaComponent },
   { path: '', redirectTo: 'iniciar-sesion', pathMatch: 'full' },
   { path: '**', component: ErrorNoEncontradoComponent }, //Sitio Web No Encontrado 404
 ];
