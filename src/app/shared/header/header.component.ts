c80cd72c86e5abdd30cac21cf101ce4647b88297
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-02-23 18:39:09 -0500

    cambiando el mensaje de inicio dependiendo del estado de su logeo

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  verificadorBool: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.verifyLooged();
   }
 
+  ngOnInit(): void {}
+
+  verifyLooged() {
+    if (localStorage.getItem('contrasena')) {
+      this.verificadorBool = true;
+    } else {
+      this.verificadorBool = false;
+    }
+  }
+  borrarContra() {
+    localStorage.removeItem('contrasena');
+    this.verificadorBool = false;
+    location.reload();
+  }
 }
